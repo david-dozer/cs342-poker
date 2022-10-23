5ad69d60519f5ed55d3c6096b35afeea731a10ab
DarshanShetUIC <dshet2@uic.edu>
2022-10-23 14:34:27 -0500

    resized all cards; added new cards; created gui gridpane for all hands, still testing

--- src/main/java/PokerMain.java
+++ src/main/java/PokerMain.java
@@ -10,6 +10,8 @@ import javafx.geometry.Pos;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 
 public class PokerMain extends Application {
@@ -18,60 +20,77 @@ public class PokerMain extends Application {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
-
+	
 	//feel free to remove the starter code from this method
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("3 Card Poker");
 		
-		// Create objects in GUI
-		BorderPane borderPane = new BorderPane();
-		Button button1 = new Button("Menu");
-		Button button2 = new Button("button 2");
-		TextField textfield1 = new TextField();
-		TextField textfield2 = new TextField();
-		
-		//Modify GUI objects before window launches
-		textfield1.setPromptText("enter text here then press button 1");
-		textfield2.setEditable(false);
-		borderPane.setTop(button1);
-		borderPane.setLeft(button2);
-		//borderPane.setCenter(textfield1);
-		borderPane.setRight(textfield2);
+		// Create grid pane along with all GUI objects
+		GridPane gridPane = new GridPane();
+		Button menuButton = new Button("Menu");
+		Image card1 = new Image("/4/poker-playing-card-4-club-miroslav-nemecek.jpg");
+		Image card2 = new Image("/4/poker-playing-card-4-diamond-miroslav-nemecek.jpg");
+		Image card3 = new Image("/4/poker-playing-card-4-heart-miroslav-nemecek.jpg");
+		Image card4 = new Image("/5/poker-playing-card-5-club-miroslav-nemecek.jpg");
+		Image card5 = new Image("/5/poker-playing-card-5-diamond-miroslav-nemecek.jpg");
+		Image card6 = new Image("/5/poker-playing-card-5-heart-miroslav-nemecek.jpg");
+		Image card7 = new Image("/6/poker-playing-card-6-club-miroslav-nemecek.jpg");
+		Image card8 = new Image("/6/poker-playing-card-6-diamond-miroslav-nemecek.jpg");
+		Image card9 = new Image("/6/poker-playing-card-6-heart-miroslav-nemecek.jpg");
+		ImageView dealerCard1 = new ImageView(card1);
+		ImageView dealerCard2 = new ImageView(card2);
+		ImageView dealerCard3 = new ImageView(card3);
+		ImageView player1Card1 = new ImageView(card4);
+		ImageView player1Card2 = new ImageView(card5);
+		ImageView player1Card3 = new ImageView(card6);
+		ImageView player2Card1 = new ImageView(card7);
+		ImageView player2Card2 = new ImageView(card8);
+		ImageView player2Card3 = new ImageView(card9);
 
+		
 		//Custom attributes to make GUI look better
-		button1.setMinHeight(250);
-		button1.setMinWidth(100);
-		button2.setMinHeight(250);
-		button2.setMinWidth(100);
-		textfield2.setMinWidth(300);
-		VBox vbox = new VBox(textfield1);
-		borderPane.setCenter(vbox);
-		borderPane.setBackground(new Background(new BackgroundFill(Color.web("#008080", 0.5), CornerRadii.EMPTY, Insets.EMPTY)));
+		menuButton.setMinHeight(40);
+		menuButton.setMinWidth(75);
+		gridPane.setBackground(new Background(new BackgroundFill(Color.web("#008080", 0.5), CornerRadii.EMPTY, Insets.EMPTY)));
+		gridPane.setGridLinesVisible(true);
+		gridPane.setHgap(10);
+		gridPane.setVgap(10);
+		//gridPane.setPadding(new Insets(0, 0, 0, 0));
 
+
+		// Set positions for all grid pane objects
+		//gridPane.add(menuButton,0,0);
+		gridPane.add(dealerCard1,2,0);
+		gridPane.add(dealerCard2,3,0);
+		gridPane.add(dealerCard3,4,0);
+		
+		gridPane.add(player1Card1,0,5);
+		gridPane.add(player1Card2,1,5);
+		gridPane.add(player1Card3,2,5);
+		
+		gridPane.add(player2Card1,4,5);
+		gridPane.add(player2Card2,5,5);
+		gridPane.add(player2Card3,6,5);
+		
 		
 		//Set event handlers for objects
-		button1.setOnAction(new EventHandler<ActionEvent>(){
+		menuButton.setOnAction(new EventHandler<ActionEvent>(){
 			@Override public void handle(ActionEvent e){
-				String s = textfield1.getText();
-				s = s + ": from the center text field!";
-				textfield2.setText(s);
-				button1.setDisable(true);
-				button1.setText("pressed");
+				menuButton.setText("Menu!");
 			}
-		});
+		});	
 		
-		button2.setOnAction(event -> {textfield1.setText("");
-				textfield2.setText("final string goes here");
-				button1.setDisable(false);
-				button1.setText("button one");});
-		
-		
-		
+		int windowWidth = 1100;
+		int windowHeight = 600;
 		// Add GUI objects to window and display
-		Scene scene = new Scene(borderPane, 700,500);
+		Scene scene = new Scene(gridPane, windowWidth,windowHeight);
+		primaryStage.setTitle("3 Card Poker");
 		primaryStage.setScene(scene);
+		primaryStage.setMinHeight(windowHeight);
+		primaryStage.setMinWidth(windowWidth);
+		primaryStage.setMaxHeight(windowHeight);
+		primaryStage.setMaxWidth(windowWidth);
 		primaryStage.show();
 	}
 
