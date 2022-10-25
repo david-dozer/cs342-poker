dc678a40b977426404c20f34a2ef0c97af452714
David Mendoza <95949823+david-dozer@users.noreply.github.com>
2022-10-25 14:25:44 -0500

    Update Card.java

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,11 +1,14 @@
-import java.util.Random;
-
 public class Card {
 	char suit;
 	int value;
+	String url; // each card retrieved image
 	
 	public Card(char suit, int value) {
 		this.suit = suit;
 		this.value = value;
+	}
+	
+	public void setURL(String url) {
+		this.url = url;
 	}
 };
