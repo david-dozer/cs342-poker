02a6aec77a447ccba4691a1fb54815bb8f77577f
David Mendoza <95949823+david-dozer@users.noreply.github.com>
2022-10-25 14:52:58 -0500

    Add files via upload

---
+++ src/main/java/Player.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Player {
+	
+	ArrayList<Card> hand;
+	int anteBet;
+	int playBet;
+	int pairPlusBet;
+	int totalWinnings;
+	
+	Player() {
+		this.hand = hand;
+		this.anteBet = anteBet;
+		this.playBet = playBet;
+		this.pairPlusBet = pairPlusBet;
+		this.totalWinnings = totalWinnings;
+	}
+}
