81ad36ccfa6abefc3d5917577f9a29cdea423175
David Mendoza <95949823+david-dozer@users.noreply.github.com>
2022-10-26 16:49:21 -0500

    Add files via upload

---
+++ src/main/java/Dealer.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Dealer {
+	// the dealer possesses the card and the deck
+	Deck theDeck;
+	ArrayList<Card> dealersHand;
+	String urls[];
+
+	Dealer() {
+		theDeck = new Deck(urls);
+		dealersHand = new ArrayList<Card> ();
+	}
+	
+	// getters for deck and dealer hand?
+	
+	public ArrayList<Card> dealHand() {
+		if (theDeck.size < 34) { theDeck.NewDeck(urls);}  // new deck
+		ArrayList<Card> h = new ArrayList<Card>();
+		for (int i = 0; i < 3; i++) {
+			h.add(theDeck.remove(0));  // remove returns element removed from collection
+			theDeck.size--;
+		}
+		return h;
+	}
+	
+};
---
+++ src/main/java/Deck.java
@@ -0,0 +1,44 @@
+// import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Deck extends ArrayList<Card> {
+	int size;
+	// Card[] deckOfCards = new Card[size];
+	
+	Deck(String urls[]) {
+		size = 52;
+		int count = 0;
+        char[] suits = {'D', 'C', 'H', 'S'};
+        int[] vals ={2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        // cards 2-10, 11-13 are Jack Queen King, respectively, 14 is Ace, highest card
+
+        for (char s : suits) {
+            for (int v : vals) {  // in arrayList, not bracket operator, but get
+            	this.add(new Card(s, v)); this.get(count).setURL(urls[count]);  
+                count++;
+            }
+        }
+        Collections.shuffle(this);
+    }
+	
+	
+	void NewDeck(String urls[]) {
+		this.clear();
+		size = 52;
+		int count = 0;
+        char[] suits = {'D', 'C', 'H', 'S'};
+        int[] vals ={2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+        // cards 2-10, 11-13 are Jack Queen King, respectively, 14 is Ace, highest card
+
+        for (char s : suits) {
+            for (int v : vals) {  // in arrayList, not bracket operator, but get
+            	this.add(new Card(s, v)); this.get(count).setURL(urls[count]);  
+                count++;
+            }
+        }
+        Collections.shuffle(this);
+	}
+	
+};
