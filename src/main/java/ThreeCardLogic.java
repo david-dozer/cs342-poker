c8b20fe1e3df512c33ece99c851fc773ef2fe77b
David Mendoza <95949823+david-dozer@users.noreply.github.com>
2022-10-29 23:37:11 -0500

    Update ThreeCardLogic.java

--- src/main/java/ThreeCardLogic.java
+++ src/main/java/ThreeCardLogic.java
@@ -16,6 +16,10 @@ public class ThreeCardLogic {
 		if (num1 == num2) {return true;} return false;
 	}
 	
+	public static boolean allSameVal(int num1, int num2, int num3) {
+		if (num1 == num2 && num2 == num3) {return true;} return false;
+	}
+	
 	public static boolean inSequence(ArrayList<Integer> hand) {
 		if (hand.get(0) == 2 && hand.get(1) == 3 && hand.get(2) == 14) { return true;}
 		// ^^ wrap around straight, ace can be used as low card one
@@ -37,26 +41,24 @@ public class ThreeCardLogic {
 		ArrayList<Integer> CardVals = sortCardValues(hand);
 		int eval = 0; // 0 is high card, 1 is SF, 2 is 3 O Kind, 3 is straight,
 				  // 4 is flush, 5 is pair
-		if (sameVal(hand.get(0).value, hand.get(1).value) || // pair case
-				sameVal(hand.get(2).value, hand.get(1).value)  ||
-				sameVal(hand.get(2).value, hand.get(0).value)) {
-			eval = 5;
+		if (sameVal(CardVals.get(0), CardVals.get(1)) || 
+				sameVal(CardVals.get(2), CardVals.get(1)) ||
+				sameVal(CardVals.get(2), CardVals.get(0))) { // pair and 3 of kind case
+			if (allSameVal(CardVals.get(0), CardVals.get(1), CardVals.get(2))) {
+				eval = 2;
+			} else {eval = 5;}
 		} else if (sameSuit(hand)) {
 			eval = 4;
 		} else if (inSequence(CardVals) && !sameSuit(hand)) {
 //				System.out.printf("STRAIGHT INSHALLAH");
 				eval = 3;
-		} else if (sameVal(hand.get(0).value, hand.get(1).value) && // 3 of kind
-				sameVal(hand.get(2).value, hand.get(1).value)  &&
-				sameVal(hand.get(2).value, hand.get(0).value)) {
-			eval = 2;
-		} else if (inSequence(CardVals) && sameSuit(hand)) { eval = 1;}
+		}
 		else if (!inSequence(CardVals) && !sameSuit(hand)) {
 //			System.out.printf("not anything");
-			eval = 0;}
+			eval = 0;
+		}
+		if (inSequence(CardVals) && sameSuit(hand)) { eval = 1;}
 		return eval;
-		// 0 is high card, 1 is SF, 2 is 3 O Kind, 3 is straight,
-	    // 4 is flush, 5 is pair
 	}
 	
 	public static int evalPPWinnings(ArrayList<Card> hand, int bet) {
@@ -76,56 +78,67 @@ public class ThreeCardLogic {
 		return (factor * bet) + bet;
 	}
 	
-	public static int compHighCards(ArrayList<Card> player, ArrayList<Card> dealer) {
-		int comp = 0;
-		ArrayList<Integer> p1Val = sortCardValues(player);
-		ArrayList<Integer> p2Val = sortCardValues(dealer);
-		for (int i = 0; i < 3; i++) {
-			if (p1Val.get(i) == p2Val.get(i)) {  // if high cards equal, move on to next
-				player.remove(i); dealer.remove(i);  // remove from deck
-			} else { break;}  // not equal, stop
-		}
-		if (p1Val.isEmpty() && p2Val.isEmpty()) {return 0;}
-		
-		if (p1Val.get(0) > p2Val.get(0)) { 
-			comp = 2;
-		} else {comp = 1;}  // dealer won
-		return comp;
-	}
+	private static int compHighCards(ArrayList<Card> player, ArrayList<Card> dealer, int topCardIndex) {
+        // get highest card at index
+        if (topCardIndex > 3) {
+            return 0;        // Out of bounds, push
+        }
+        if (topCardIndex <= 0) {
+            return -123;     // ERROR, return error value
+        }
+        
+        ArrayList<Integer>playerVals = sortCardValues(player);
+        ArrayList<Integer>dealerVals = sortCardValues(dealer);
+        
+        if (dealerVals.get(3-topCardIndex) < 12) {  // dealer no have queen or higher
+        	return 0;
+        }
+        
+        // Highest card is last in list, keep going until vals not equal
+        int dealerHigh = dealerVals.get(3-topCardIndex);
+        int playerHigh = playerVals.get(3-topCardIndex);
+        
+        if (dealerHigh == playerHigh) {
+            return compHighCards(dealer, player, topCardIndex++);  // Eval based on next highest card
+        } 
+        
+        if (dealerHigh > playerHigh) {return 1;} else {return 2;}
+    }
 	
+	// 0, nobody wins, 1, dealer wins, 2, player wins
 	public static int compareHands(ArrayList<Card> dealer, ArrayList<Card> player) {
-//		0 if neither hand won
-//		• 1 if the dealer hand won
-//		• 2 if the player hand won
-		int comp = 0;  // to be returned
 		int playerEval = evalHand(player);
 		int dealerEval = evalHand(dealer);
 		
-		if (playerEval != 0 && dealerEval != 0) {  // types of hands are found not high card
-			if (playerEval < dealerEval) {comp = 2;} 
-			else if (dealerEval < playerEval) {comp = 1;} 
-			else if (playerEval == dealerEval) {  // program high card for flush, 
-				// in same type of hand
-				if (playerEval != 2 && dealerEval != 2) { // 3, 4, or 1
-					int sumPlayer = 0, sumDealer = 0;
-					for (Card p : player) {
-						sumPlayer = sumPlayer + p.getValue();
-					}
-					for (Card d : dealer) {
-						sumDealer = sumDealer + d.getValue();
-					}
-					if (sumPlayer > sumDealer) {comp = 2;}
-					else if (sumDealer > sumPlayer) {comp = 1;}
-					else if (sumPlayer == sumDealer) { comp = 0;}
-				} else { // flushes, so highest card wins
-					comp = compHighCards(player, dealer);
+		if (dealerEval < playerEval) {
+			if (dealerEval != 0) {  // lower the eval, better the hand
+				System.out.printf("went to here, dealer wins\n");
+				return 1;} else {return 2;}}  // 0 is basic hand, auto lose
+		else if (playerEval < dealerEval) {
+			if (playerEval != 0) {  // lower the eval, better the hand
+				System.out.printf("went to here, player wins\n");
+				return 2;} else {return 1;}}  // 0 is basic hand, auto lose
+		
+		else {  // if equal
+			System.out.printf("went to here, same typa hands\n");
+			if (dealerEval == 5 || dealerEval == 2 || dealerEval == 3
+					|| dealerEval == 1) {  // pair, 3 of kind, straight, and SF.
+				// can sum up all values to determine more powerful hand
+				int sumPlayer = 0, sumDealer = 0;
+				for (Card p : player) {
+					sumPlayer = sumPlayer + p.getValue();
 				}
+				for (Card d : dealer) {
+					sumDealer = sumDealer + d.getValue();
+				}
+				// sum comparing handling
+				if (sumPlayer > sumDealer) {return 2;}
+				else if (sumDealer > sumPlayer) {return 1;}
+				else if (sumPlayer == sumDealer) { return 0;}
+			} else if (dealerEval == 0 || dealerEval == 4) {  // with normal and flush, find high card
+				return compHighCards(player, dealer, 1);
 			}
-			else if(playerEval == 0 && dealerEval != 0) { comp = 1;}
-			else if(playerEval != 0 && dealerEval == 0) { comp = 2;} 
-		} else if (playerEval == 0 && dealerEval == 0) {  // high card scenario
-			comp = compHighCards(player, dealer);
 		}
-		return comp;
+		return 0;
 	}
 };
